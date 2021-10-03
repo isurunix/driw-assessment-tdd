8d10ed409552b44981986b94a921404b68ef7918
Isuru Buddhika Pathirana <isurunix@gmail.com>
2021-10-03 13:54:14 +0530

    Initial project setup

---
+++ src/main/java/dev/isurubuddhika/driw/DriwApplication.java
@@ -0,0 +1,13 @@
+package dev.isurubuddhika.driw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/isurubuddhika/driw/DriwApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.isurubuddhika.driw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
